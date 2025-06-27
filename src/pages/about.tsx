6949c959516892a4dbcfcc94ea8823fe1f832841
Immanuel Phillips <immanuelphillips022@gmail.com>
2025-06-27 18:57:51 +0000

    promo pack + about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,9 +8,19 @@ export default function About() {
       <div className="container">
         <div className='pt-5 lg:pt-24 pb-40 px-4'>
             <div className=" pb-8 mb:pb-16">
-                <h1 className="text-3xl md:text-7xl text-[#520B1F] font-bold ">this is the <br/> corporate brand tagline.</h1>
+                <h1 className="text-3xl md:text-7xl text-[#520B1F] font-bold ">BCARTGH - 
+                <br/> Limitless CREATIVITY
+                </h1>
                 <div className='pt-10 pb-2'><img src="/img/banner-2.webp" alt="logo" className="w-full h-full rounded-3xl" /></div>
-                <div className="pt-10 text-sm ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nibh dui, vulputate ut est sed, ornare auctor ante. Praesent orci dui, accumsan ac odio sed, elementum varius turpis. Maecenas rhoncus condimentum mollis. Etiam id rutrum velit. Nam dictum vitae leo ut vehicula. Integer at fringilla velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                <div className="pt-10 text-sm ">
+                At BCARTGH, we believe that creativity is not just an expression — it’s a movement. Our mission is to drive transformation through technology by building a dynamic digital platform that empowers photographers, designers, artists, and content creators across Africa and beyond.
+
+We are not just a marketplace — we are a community and catalyst for creative growth. Whether you’re capturing the essence of everyday life, designing captivating visuals, or telling immersive digital stories, BCARTGH provides the tools, exposure, and support to help you succeed.
+
+From seamless content showcasing and direct-to-audience selling, to brand collaborations and monetization features, we exist to make sure your art is seen, valued, and rewarded. Creativity knows no bounds, and at BCARTGH, neither do you.
+                    
+                    At BCARTGH, we believe creativity knows no bounds. Our platform empowers photographers, designers, artists, and content creators to showcase, sell, and thrive. Whether you're capturing everyday beauty or building visual stories, BCART is your space to be seen, supported, and celebrated.
+
                 </div>
             </div>
             {/* section 2 */}
@@ -21,22 +31,24 @@ export default function About() {
                         <div className=''>
                             <div className='text-xl md:text-3xl font-bold pb-5 text-[#520B1F]'>Our Mission</div>
                             <div className='text-sm tracking-wider'>
-                            This is an introductory text stating who we are and what we do. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nibh dui, vulputate ut est sed, ornare auctor ante. Praesent orci dui, accumsan ac odio sed, elementum varius turpis. Maecenas rhoncus condimentum mollis. Etiam id rutrum velit. Nam dictum vitae leo ut vehicula. Integer at fringilla velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                            </div>
+                            <div className='mb-4'>1. Our mission is to amplify Ghana’s visual & creative talent—making professional photography and videography accessible to businesses and individuals.</div>
+
+<div className='mb-4'>2. Enriching lives globally through our carefully curated board and card games</div>
+</div>
                         </div>
                         <div className=' pt-10 lg:pt-16'>
                             <div className='text-xl md:text-3xl font-bold pb-5 text-[#520B1F]'>Our Vision</div>
                             <div className='text-sm tracking-wider'>
-                            This is an introductory text stating who we are and what we do. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nibh dui, vulputate ut est sed, ornare auctor ante. Praesent orci dui, accumsan ac odio sed, elementum varius turpis. Maecenas rhoncus condimentum mollis. Etiam id rutrum velit. Nam dictum vitae leo ut vehicula. Integer at fringilla velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                            </div>
+                            We envision a creative ecosystem where talented photographers, videographers, and game creators from Ghana and beyond are discovered, supported, and celebrated. Through Bcart, we aim to redefine how Africa’s stories are told—authentically, beautifully, and globally accessible.</div>
                         </div>
                     </div>
                     <div className='lg:col-span-2'>
                     <div className='lg:px-32'>
-                            <div className='text-xl md:text-3xl font-bold pb-5 text-[#520B1F]'>Our Mission</div>
+                            <div className='text-xl md:text-3xl font-bold pb-5 text-[#520B1F]'>Our Dedication</div>
                             <div className='text-sm tracking-wider'>
-                            This is an introductory text stating who we are and what we do. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nibh dui, vulputate ut est sed, ornare auctor ante. Praesent orci dui, accumsan ac odio sed, elementum varius turpis. Maecenas rhoncus condimentum mollis. Etiam id rutrum velit. Nam dictum vitae leo ut vehicula. Integer at fringilla velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                            </div>
+                            At BCARTGH, we believe creativity knows no bounds. Our platform empowers photographers, designers, artists, and content creators to showcase, sell, and thrive. Whether you're capturing everyday beauty or building visual stories, BCART is your space to be seen, supported, and celebrated.
+
+</div>
                         </div>
                         <div className='flex justify-center pt-24'><img src="/img/about-2.webp" alt="logo" className=" rounded-3xl" /></div>
                     </div>
@@ -51,22 +63,25 @@ export default function About() {
                             <div className='absolute bottom-40'>
                                 <img src="/img/team-1.webp" alt="logo" className="w-full h-full" />
                             </div>
-                            <div className='font-bold text-lg pt-44 text-[#373F41]'>Firstname Lastname</div>
-                            <div className=' text-lg pt-5 pb-4 text-[#373F41]'>Position</div>
+                            <div className='font-bold text-lg pt-44 text-[#373F41]'>Benita Copperfield</div>
+                            <div className=' text-lg pt-5 pb-4 capitalize text-[#373F41]'>CEO & Lead creative
+                            </div>
                         </div>
                         <div className='bg-[#520B1F] bg-opacity-20 rounded-3xl w-full p-6 flex flex-col items-center relative'>
                             <div className='absolute bottom-40'>
                                 <img src="/img/team-2.webp" alt="logo" className="w-full h-full" />
                             </div>
-                            <div className='font-bold text-lg pt-44 text-[#373F41]'>Firstname Lastname</div>
-                            <div className=' text-lg pt-5 pb-4 text-[#373F41]'>Position</div>
+                            <div className='font-bold text-lg pt-44 text-[#373F41]'>Xane Asiamah</div>
+                            <div className=' text-lg pt-5 pb-4 capitalize text-[#373F41]'>lead Marketer & head OF Creative arts
+                            </div>
                         </div>
                         <div className='bg-[#520B1F] bg-opacity-20 rounded-3xl w-full p-6 flex flex-col items-center relative'>
                             <div className='absolute bottom-40'>
                                 <img src="/img/team-3.webp" alt="logo" className="w-full h-full" />
                             </div>
-                            <div className='font-bold text-lg pt-44 text-[#373F41]'>Firstname Lastname</div>
-                            <div className=' text-lg pt-5 pb-4 text-[#373F41]'>Position</div>
+                            <div className='font-bold text-lg pt-44 text-[#373F41]'>Kafui Fialor</div>
+                            <div className=' text-lg pt-5 pb-4 capitalize text-[#373F41]'>lead Developer & head OF production
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/pages/promo-pack.tsx
+++ src/pages/promo-pack.tsx
@@ -39,7 +39,7 @@ export default function PromoPack() {
                                     We’re sorry you couldn’t find what you are looking for. Feel free to tell us what you want and our creatives will make your wishes come true
                                 </div>
                                 <div className='pt-7 text-xl font-semibold text-[#2B1139]'>
-                                GH₵ 20.00 / month
+                                    GH₵ 20.00 / month
                                 </div>
                             </div>
                         </div>
@@ -70,7 +70,10 @@ export default function PromoPack() {
                     <div className="pb-8 mb:pb-16">
                         <h1 className="text-3xl md:text-7xl text-[#520B1F] font-bold ">Promo packs</h1>
                         <div className="pt-10 text-sm">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nibh dui, vulputate ut est sed, ornare auctor ante. Praesent orci dui, accumsan ac odio sed, elementum varius turpis. Maecenas rhoncus condimentum mollis. Etiam id rutrum velit. Nam dictum vitae leo ut vehicula. Integer at fringilla velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                            <div> At BCARTGH, we understand that visibility is currency in the creative world. That’s why we created Promo Packs — tailor-made promotional bundles designed to help creatives grow their audience, boost engagement, and increase sales.
+                                Whether you're launching a new photo series, unveiling a product line, or promoting a digital collection, our Promo Packs offer the right mix of exposure, tools, and insights to help you stand out.</div>
+
+                            <div>Promo Packs are more than marketing — they’re about elevating your work to the right audience and helping you thrive in a crowded digital space. Because at BCARTGH, your creativity deserves to be celebrated — loudly, proudly, and widely.</div>
                         </div>
                         <div className='pt-24'>
                             <div className='flex flex-col md:flex-row items-center justify-between gap-4'>
@@ -78,11 +81,19 @@ export default function PromoPack() {
                                     <img src="/img/about-1.webp" alt="logo" className="w-24 md:w-60" />
                                 </div>
                                 <div className='w-full md:w-1/2'>
-                                    <div className='text-xl md:text-3xl font-bold pb-5 text-[#520B1F]'>Package 1</div>
+                                    <div className='text-xl md:text-3xl font-bold pb-2 text-[#520B1F]'>Starter Snap</div>
+                                    <div className='text-base md:text-lg font-bold pb-5 text-[#520B1F]'>GHS 350 / $35</div>
                                     <div className='text-sm tracking-wider'>
-                                        This is an introductory text stating who we are and what we do. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nibh dui, vulputate ut est sed, ornare auctor ante. Praesent orci dui, accumsan ac odio sed, elementum varius turpis. Maecenas rhoncus condimentum mollis. Etiam id rutrum velit. Nam dictum vitae leo ut vehicula. Integer at fringilla velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                                    </div>
-                                    <div className='pt-7'>
+                                        Perfect for portraits, birthdays, and quick creative sessions, this package is designed for those who want to capture special moments without the fuss. Whether it's a solo portrait, a birthday milestone, or a quick creative shoot, this pack offers just the right balance of time and quality.
+                                        <ul className='list-disc list-inside pt-5'>
+                                            <li className='mb-2'>30-minute session</li>
+                                            <li className='mb-2'>5 professionally edited images</li>
+                                            <li className='mb-2'>Ideal for individuals, kids, or small groups looking for high-impact, short-format photography.</li>
+
+                                        </ul>
+
+                                        Capture it fast. Keep it forever.
+                                    </div>                                    <div className='pt-7'>
                                         <button
                                             className='bg-[#520B1F] text-white px-10 py-3 text-sm font-bold rounded-full'
                                             onClick={toggleSidebar}>
@@ -98,9 +109,75 @@ export default function PromoPack() {
                                     <img src="/img/about-1.webp" alt="logo" className="w-24 md:w-60" />
                                 </div>
                                 <div className='w-full md:w-1/2'>
-                                    <div className='text-xl md:text-3xl font-bold pb-5 text-[#520B1F]'>Package 2</div>
+                                    <div className='text-xl md:text-3xl font-bold pb-2 text-[#520B1F]'>Pro Storyteller</div>
+                                    <div className='text-base md:text-lg font-bold pb-5 text-[#520B1F]'>GHS 600 / $60</div>
                                     <div className='text-sm tracking-wider'>
-                                        This is an introductory text stating who we are and what we do. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nibh dui, vulputate ut est sed, ornare auctor ante. Praesent orci dui, accumsan ac odio sed, elementum varius turpis. Maecenas rhoncus condimentum mollis. Etiam id rutrum velit. Nam dictum vitae leo ut vehicula. Integer at fringilla velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                        Designed for those who want more than just a photo — the <strong>Pro Storyteller</strong> pack is perfect for portraits, birthdays, or any creative shoot where storytelling is key. With extended time and added visuals, it offers a polished, high-impact capture of your moment.
+                                        <ul className='list-disc list-inside pt-5'>
+                                            <li className='mb-2'>1.5 hours session</li>
+                                            <li className='mb-2'>15 professionally edited images</li>
+                                            <li className='mb-2'>1 minute video reel</li>
+                                            <li className='mb-2'>Ideal for individuals, kids, or small groups looking for high-impact, premium photography.</li>
+
+                                        </ul>
+                                        High-impact capture of your moment
+
+                                    </div>
+                                    <div className='pt-7'>
+                                        <button
+                                            className='bg-[#520B1F] text-white px-10 py-3 text-sm font-bold rounded-full'
+                                            onClick={toggleSidebar}>
+                                            Get Started
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='pt-24'>
+                            <div className='flex flex-col md:flex-row items-center justify-between gap-4'>
+                                <div className='flex justify-center items-center w-full md:w-1/2'>
+                                    <img src="/img/about-1.webp" alt="logo" className="w-24 md:w-60" />
+                                </div>
+                                <div className='w-full md:w-1/2'>
+                                    <div className='text-xl md:text-3xl font-bold pb-2 text-[#520B1F]'>Business Visual Pack</div>
+                                    <div className='text-base md:text-lg font-bold pb-5 text-[#520B1F]'>GHS 800 / $80</div>
+                                    <div className='text-sm tracking-wider'>
+                                    Ideal for SME businesses, content creators, and product sellers, this package is crafted to help you elevate your brand’s visual identity. Whether you're launching a new product, building your online presence, or sharing your creative process, this pack provides the right mix of content to tell your story with impact.                                        <ul className='list-disc list-inside pt-5'>
+                                            <li className='mb-2'>Includes high-quality product shots</li>
+                                            <li className='mb-2'>Behind-the-scenes coverage to showcase your process or team</li>
+                                            <li className='mb-2'>Engaging reels for social media that boost reach and connection</li>
+
+                                        </ul>
+
+                                        Perfect for brands that want to look professional, feel authentic, and stay memorable.
+                                    </div>                                    <div className='pt-7'>
+                                        <button
+                                            className='bg-[#520B1F] text-white px-10 py-3 text-sm font-bold rounded-full'
+                                            onClick={toggleSidebar}>
+                                            Get Started
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='pt-24'>
+                            <div className='flex flex-col md:flex-row-reverse items-center justify-between gap-4'>
+                                <div className='flex justify-center items-center w-full md:w-1/2'>
+                                    <img src="/img/about-1.webp" alt="logo" className="w-24 md:w-60" />
+                                </div>
+                                <div className='w-full md:w-1/2'>
+                                    <div className='text-xl md:text-3xl font-bold pb-2 text-[#520B1F]'>Event Essentials</div>
+                                    <div className='text-base md:text-lg font-bold pb-5 text-[#520B1F]'>GHS 1200 / $100</div>
+                                    <div className='text-sm tracking-wider'>
+                                    Designed to capture life’s biggest moments in full detail, the <strong>Event Essentials</strong> is ideal for weddings, parties, and special ceremonies. From candid emotions to key highlights, this package delivers a complete storytelling experience with both photo and video coverage.
+                                        <ul className='list-disc list-inside pt-5'>
+    <li className='mb-2'>2 hours of event coverage</li>
+    <li className='mb-2'>50+ professionally edited photos</li>
+    <li className='mb-2'>Full highlight video capturing the best moments</li>
+    <li className='mb-2'>Perfect for couples, families, and celebrants who want lasting memories</li>
+  </ul>
+                                        High-impact capture of your moment
+
                                     </div>
                                     <div className='pt-7'>
                                         <button
